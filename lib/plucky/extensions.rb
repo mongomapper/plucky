1bde2f6403c7f4fb5c324ddf68a117c57aacd68a
John Nunemaker <nunemaker@gmail.com>
2010-05-18 16:26:19 -0400

    Symbol operators now are equality and comparable aware.

--- lib/plucky/extensions.rb
+++ lib/plucky/extensions.rb
@@ -1,9 +1,23 @@
 class SymbolOperator
+  include Comparable
+
   attr_reader :field, :operator
 
   def initialize(field, operator, options={})
     @field, @operator = field, operator
   end unless method_defined?(:initialize)
+  
+  def <=>(other)
+    if field == other.field
+      operator <=> other.operator
+    else
+      field.to_s <=> other.field.to_s
+    end
+  end
+  
+  def ==(other)
+    field == other.field && operator == other.operator
+  end
 end
 
 class Symbol
--- test/test_symbol_operator.rb
+++ test/test_symbol_operator.rb
@@ -12,5 +12,38 @@ class SymbolOperatorTest < Test::Unit::TestCase
     should "have operator" do
       subject.operator.should == 'in'
     end
+
+    context "==" do
+      should "be true if field and operator are equal" do
+        SymbolOperator.new(:foo, 'in').should == SymbolOperator.new(:foo, 'in')
+      end
+      
+      should "be false if fields are equal but operators are not" do
+        SymbolOperator.new(:foo, 'in').should_not == SymbolOperator.new(:foo, 'all')
+      end
+      
+      should "be false if operators are equal but fields are not" do
+        SymbolOperator.new(:foo, 'in').should_not == SymbolOperator.new(:bar, 'in')
+      end
+      
+      should "be false if neither are equal" do
+        SymbolOperator.new(:foo, 'in').should_not == SymbolOperator.new(:bar, 'all')
+      end
+    end
+
+    context "<=>" do
+      should "same field, different operator" do
+        (SymbolOperator.new(:foo, 'in') <=> SymbolOperator.new(:foo, 'all')).should ==  1
+        (SymbolOperator.new(:foo, 'all') <=> SymbolOperator.new(:foo, 'in')).should == -1
+      end
+
+      should "same field same operator" do
+        (SymbolOperator.new(:foo, 'in') <=> SymbolOperator.new(:foo, 'in')).should == 0
+      end
+
+      should "different field" do
+        (SymbolOperator.new(:foo, 'in') <=> SymbolOperator.new(:bar, 'in')).should == 1
+      end
+    end
   end
 end
