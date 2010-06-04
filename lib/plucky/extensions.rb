5038a1f4d1173a93619ffb3442771e4fdb1921a0
John Nunemaker <nunemaker@gmail.com>
2010-06-04 08:29:19 -0400

    Explicitly define all the symbol methods and include them so there is a trail to follow for runtime spelunkers.

--- lib/plucky/extensions.rb
+++ lib/plucky/extensions.rb
@@ -1,25 +1,3 @@
 # encoding: UTF-8
-class SymbolOperator
-  include Comparable
-
-  attr_reader :field, :operator
-
-  def initialize(field, operator, options={})
-    @field, @operator = field, operator
-  end unless method_defined?(:initialize)
-  
-  def <=>(other)
-    if field == other.field
-      operator <=> other.operator
-    else
-      field.to_s <=> other.field.to_s
-    end
-  end
-  
-  def ==(other)
-    field == other.field && operator == other.operator
-  end
-end
-
 require 'plucky/extensions/duplicable'
 require 'plucky/extensions/symbol'
